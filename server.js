3cfcdbf4f378028e230cfae2ea18b684c09103dd
natious78 <amir.askari2123@gmail.com>
2022-09-20 11:44:02 +0430

    wtf

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ const server = app.listen(port, () => {
 });
 
 process.on('unhandledRejection', err => {
-  console.log('UNHANDLED REJECTION!! 💥 Shutting down...');
+  console.log('UNHANDLED REJECTION!!! 💥 Shutting down...');
   console.log(err.name, err.message);
   server.close(() => {
     process.exit(1);
